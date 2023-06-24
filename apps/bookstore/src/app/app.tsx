9f3195573e0c49d6fb6e8845e3c942617be86d3b
Mauricio Buffa <mauriciobuffa@MacBook-Pro-de-Mauricio.local>
2023-06-24 19:58:11 -0300

    add books feature and ui components

--- apps/bookstore/src/app/app.tsx
+++ apps/bookstore/src/app/app.tsx
@@ -1,15 +1,32 @@
-import styled from 'styled-components'
-
-const StyledApp = styled.div``
-
+import { Link, Navigate, Route, Routes } from 'react-router-dom'
+import { BooksFeature } from '@acme/books/feature'
+// importing the UI library into our App
+import {
+  GlobalStyles,
+  Header,
+  Main,
+  NavigationItem,
+  NavigationList
+} from '@acme/ui'
 export const App = () => {
   return (
-    <StyledApp>
-      <header>
+    <>
+      <GlobalStyles />
+      <Header>
         <h1>Bookstore</h1>
-      </header>
-    </StyledApp>
+        <NavigationList>
+          <NavigationItem>
+            <Link to='/books'>Books</Link>
+          </NavigationItem>
+        </NavigationList>
+      </Header>
+      <Main>
+        <Routes>
+          <Route path='/books' element={<BooksFeature />} />
+          {/* path / redirect to books */}
+          <Route path='/' element={<Navigate to='/books' />} />
+        </Routes>
+      </Main>
+    </>
   )
 }
-
-export default App
