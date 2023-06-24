a36463ca2287b4ce37807a0f40f74042f65e4e46
Mauricio Buffa <mauriciobuffa@MacBook-Pro-de-Mauricio.local>
2023-06-24 15:58:53 -0300

    setup monorepo with nx and react

--- apps/bookstore/src/app/app.tsx
+++ apps/bookstore/src/app/app.tsx
@@ -1,17 +1,15 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-import NxWelcome from './nx-welcome';
+const StyledApp = styled.div``
 
-const StyledApp = styled.div`
-  // Your style here
-`;
-
-export function App() {
+export const App = () => {
   return (
     <StyledApp>
-      <NxWelcome title="bookstore" />
+      <header>
+        <h1>Bookstore</h1>
+      </header>
     </StyledApp>
-  );
+  )
 }
 
-export default App;
+export default App
